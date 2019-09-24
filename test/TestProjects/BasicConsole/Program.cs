5a08bdc453d764dd093f1f3beed22c0a82b205d0
Niels Tanis <ntanis@veracode.com>
2019-09-24 21:24:24 +0200

    ReOrg source + testcases

---
+++ test/TestProjects/BasicConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var taint = args[0];
+
+            if (System.IO.File.Exists(taint))
+            {
+                var x = System.IO.File.OpenRead(taint);
+            }
+
+            if (System.IO.File.Exists("test"))
+            {
+                var u = System.IO.File.OpenRead("test");
+            }
+        }
+    }
+}
